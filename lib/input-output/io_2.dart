5381f636d2a20d93e35b918bc99d90db887bebf4
Muhamad Ikhsan <iksan.qren@gmail.com>
2024-04-09 17:13:26 +0700

    update model variable  ke camelCase

--- lib/input-output/io_2.dart
+++ lib/input-output/io_2.dart
@@ -2,9 +2,9 @@ import 'dart:io';
 
 void main() {
   print("Masukkan nama depan kamu");
-  String? nama_depan = stdin.readLineSync();
+  String? namaDepan = stdin.readLineSync();
   print("Masukkan nama belakang kamu");
-  String? nama_belakang = stdin.readLineSync();
+  String? namaBelakang = stdin.readLineSync();
 
-  print("Nama lengkap kamu adalah $nama_depan $nama_belakang");
+  print("Nama lengkap kamu adalah $namaDepan $namaBelakang");
 }

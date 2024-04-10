83b81188a37231c195663d84a1b0b9915bdc033c
Muhamad Ikhsan <iksan.qren@gmail.com>
2024-04-10 14:58:46 +0700

    update loops_2.dart

--- lib/condition_loops/loops_2.dart
+++ lib/condition_loops/loops_2.dart
@@ -1,5 +1,15 @@
 void main() {
-  for (int i = 1; i < 10; i--) {
+  /** OUTPUT: saya baris ke-1, dst.
+    for (int i = 1; i > 10; i++) {
     print("hai, saya baris ke $i");
-  }
+  } 
+  **/
+
+  // OUTPUT: saya angka 80, dst.
+  /** 
+   for (int i = 10; i < 100; i++) {
+    if (i % 2 == 0) {
+      print("saya angka genap $i");
+    }
+  } **/
 }
